ae37ba36f23e5d92488d506c33a4577c71ec07ee
B00980368 <sweeney-c33@ulster.ac.uk>
2025-10-06 15:30:05 +0100

    updated code with new member class

--- Week3OOp/Program.cs
+++ Week3OOp/Program.cs
@@ -8,7 +8,16 @@ class Program
     {
         // Create a new instance (object) of the Book class
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
+        Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
+        Console.WriteLine("Currently available book:");
         book.DisplayInfo();
+        book1.DisplayInfo();
+
+        Member member = new Member(1, "John Smith", "1 High Street", 0790090090);
+        Member member1 = new Member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
---
+++ Week3OOp/member.cs
@@ -0,0 +1,48 @@
+﻿namespace Library
+{
+    class Member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+       public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } //makes it read only. can only be set in the constructor
+    }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }   
+        public int Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+
+    public Member(int memberId, string name, string address, int phone)
+    {
+        this.MemberId = memberId;
+        this.Name = name;
+        this.Address = address;
+        this.Phone = phone;
+    }
+
+    public void DisplayInfo()
+    {
+        // Print the member details to the console
+        Console.WriteLine($"Member ID: {MemberId}");
+        Console.WriteLine($"Member Name: {Name}");
+        Console.WriteLine($"Member Address: {Address}");
+        Console.WriteLine($"Member Phone: {Phone}");
+    }
+}
+}
