809cee3154da0b3deea65f1b28f60ddb97ff0f45
B00980368 <sweeney-c33@ulster.ac.uk>
2025-10-06 15:01:38 +0100

    updated code with access modifiers

---
+++ Week3OOp/Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Library;
+
+class Book
+{
+    string Title;
+    string Author;
+    string ISBN;
+
+    public Book(string bookTitle, string bookAuthor, string bookISBN)
+    {
+        Title = bookTitle;
+        Author = bookAuthor;
+        ISBN = bookISBN;
+    }
+    public void DisplayInfo()
+    {
+        // Print the book details to the console
+        Console.WriteLine($"Book title: {Title}");
+        Console.WriteLine($"Book Author: {Author}");
+        Console.WriteLine($"Book ISBN: {ISBN}");
+
+    }
+}
+
--- Week3OOp/Program.cs
+++ Week3OOp/Program.cs
@@ -1,25 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-class Book
+
+using Library;
+class Program
 {
-    string Title;
-    string Author;
-    string ISBN;
-
-    public Book(string bookTitle, string bookAuthor, string bookISBN)
-    {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
-    }
-    void DisplayInfo()
-    {
-        // Print the book details to the console
-        Console.WriteLine($"Book title: {Title}");
-        Console.WriteLine($"Book Author: {Author}");
-        Console.WriteLine($"Book ISBN: {ISBN}");
-
-    }
     static void Main(string[] args)
     {
         // Create a new instance (object) of the Book class
