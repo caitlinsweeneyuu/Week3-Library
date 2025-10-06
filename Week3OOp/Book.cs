2d125c2ea373b25ff31274a902a49d51ddba8383
B00980368 <sweeney-c33@ulster.ac.uk>
2025-10-06 15:12:03 +0100

    updated code with variables and properties

--- Week3OOp/Book.cs
+++ Week3OOp/Book.cs
@@ -2,15 +2,32 @@
 
 class Book
 {
-    string Title;
-    string Author;
-    string ISBN;
+    private string title;
+    private string author;
+    private string isbn;
 
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+
+    public string ISBN
+    {
+        get { return isbn; }
+        set { isbn = value; }
+    }
     public Book(string bookTitle, string bookAuthor, string bookISBN)
     {
-        Title = bookTitle;
-        Author = bookAuthor;
-        ISBN = bookISBN;
+        this.Title = bookTitle;
+        this.Author = bookAuthor;
+        this.ISBN = bookISBN;
     }
     public void DisplayInfo()
     {
